2df4ca706e82ac9e32c473882736ae7f807a9ddd
MatteoGenovese <matteo.genovese@icloud.com>
2022-09-19 18:14:04 +0200

    feat: table for fake data

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -3,7 +3,44 @@
 @section('content')
 
 
-<h1>index</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">id</th>
+                        <th scope="col">Author</th>
+                        <th scope="col">Title</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($posts as $post)
+                        <tr>
+                            <th scope="row">{{ $post->id }}</th>
+                            <td>{{ $post->author }}</td>
+                            <td>{{ $post->title }}</td>
+                            <td>
+                                <a href="" class="btn btn-sm btn-success">
+                                    Edit
+                                </a>
+                                <a href="" class="btn. btn-sm btn-danger">
+                                    Delete
+                                </a>
 
+                            </td>
+                        </tr>
+                    @empty
+
+                    @endforelse
+
+                </tbody>
+              </table>
+
+
+        </div>
+    </div>
+</div>
 
 @endsection
