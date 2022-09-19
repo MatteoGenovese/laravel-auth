fc90b55b90da857d0e6061eff87b700d7720ba27
MatteoGenovese <matteo.genovese@icloud.com>
2022-09-19 18:13:25 +0200

    feat: pass all the fake posts

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Auth;
@@ -17,7 +18,8 @@ class PostController extends Controller
     public function index()
     {
         //
-        return view('admin.posts.index');
+        $posts = Post::all();
+        return view('admin.posts.index',compact('posts'));
     }
 
     /**
