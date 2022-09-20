e4d1c3cd86d8fb4cddef4eb74951621d794b4cd3
MatteoGenovese <matteo.genovese@icloud.com>
2022-09-20 12:34:20 +0200

    feat: form for create

---
+++ resources/views/includes/form.blade.php
@@ -0,0 +1,21 @@
+<div class="form-group">
+    <label for="title">title</label>
+    <input type="text" class="form-control" id="title"  placeholder="Enter title" name="title">
+</div>
+<div class="form-group">
+    <label for="author">author</label>
+    <input type="text" class="form-control" id="author"  placeholder="Enter author" name="author">
+</div>
+<div class="form-group">
+    <label for="post_date">post date</label>
+    <input type="date" class="form-control" id="post_date"  placeholder="Enter the post date" name="post_date">
+</div>
+<div class="form-group">
+    <label for="post_image">post image</label>
+    <input type="text" class="form-control" id="post_image"  placeholder="Enter the post image" name="post_image">
+</div>
+<div class="form-group">
+    <label for="post_content">post content</label>
+    <input type="text" class="form-control" id="post_content"  placeholder="Enter the post content" name="post_content">
+</div>
+<button type="submit" class="btn btn-primary">Submit</button>
