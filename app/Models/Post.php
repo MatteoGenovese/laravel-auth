f5713c0d273001efcbe145dd7f306d56827d086d
MatteoGenovese <matteo.genovese@icloud.com>
2022-09-20 12:31:35 +0200

    feat: Post Model fillable

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'author',
+        'post_date',
+        'post_image',
+        'post_content'
+    ];
 }
